6564c0bc7611d39d1398c42e44c4653e085a1ef8
Joseph Melettukunnel <jmelett@gmail.com>
2015-06-01 02:48:29 +0200

    Update __init__.py

--- trader/__init__.py
+++ trader/__init__.py
@@ -2,8 +2,8 @@
 pyFxTrader
 """
 
-__author__ = 'Joseph Melettuunnel'
-__email__ = 'jmelett@gmail.com'
+__author__ = 'Joseph Melettukunnel, Jonathan Stoppani'
+__email__ = 'jmelett@gmail.com, jonathan@stoppani.name'
 __version__ = '0.0.1'
 __url__ = 'https://github.com/jmelett/pyFxTrader'
 __license__ = 'MIT'
