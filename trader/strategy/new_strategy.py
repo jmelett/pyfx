7aaee453127eef791033e5ce169bb01361ca783e
Joseph Melettukunnel <jmelett@gmail.com>
2016-12-22 18:54:07 +0100

    Use self. _tick_tf_time_check (#4)

--- trader/strategy/new_strategy.py
+++ trader/strategy/new_strategy.py
@@ -16,11 +16,11 @@ from ..portfolio import Close, Open
 
 
 class NewStrategy(StrategyBase):
-    '''
-    A strategy for demonstrating how a strategy should be built.
+    """A strategy for demonstrating how a strategy should be built.
+
     This strategy simply closes a postion, if there is an open position, and
     opens a position at 5 minutes intervals if new candles exist.
-    '''
+    """
     time_delta = timedelta(seconds=1)
     timeframe_delta = {
         'H2': timedelta(minutes=120),
@@ -28,8 +28,8 @@ class NewStrategy(StrategyBase):
         'M15': timedelta(minutes=15),
         'M5': timedelta(minutes=5),
     }
-    mode = 'backtest' ## or set to 'live'
-    tick_tf = 'M5' ## keep it at 'M5' until later versions
+    mode = 'backtest'  # or set to 'live'
+    tick_tf = 'M5'  # keep it at 'M5' until later versions
     timeframes = ['M5', 'M15', 'H1', 'H2']
     buffer_size = 300
     sma_intervals = {
@@ -38,29 +38,27 @@ class NewStrategy(StrategyBase):
     }
 
     def start(self, broker, tick):
-
         self.check_timeframes()
-        super(NewStrategy, self).start(broker,tick)
+        super(NewStrategy, self).start(broker, tick)
         self.feeds = OrderedDict()
         self.last_tick = tick
         self.last_ticks = {tf: self.last_tick for tf in self.timeframes}
-        self.last_candles = {tf: self.last_tick - self.time_delta \
-            for tf in self.timeframes}
+        self.last_candles = {tf: self.last_tick - self.time_delta for tf in
+                             self.timeframes}
 
     def check_timeframes(self):
-        '''Ensure timeframes are in ascending order'''
+        """ Ensure timeframes are in ascending order """
         tf_order = ['M5', 'M15', 'H1', 'H2']
         timeframes = [tf for tf in tf_order if tf in self.timeframes]
 
         if self.timeframes != timeframes:
-            raise Exception("Timeframes list in strategy must be in ascending \
-order in length.")
+            raise Exception("Timeframes list in strategy must be in ascending "
+                            "order in length.")
 
     def _tick_tf_time_check(self, tick):
-        '''
-        Check if enough ticks have passed for new tick_tf candle or new
+        """Check if enough ticks have passed for new tick_tf candle or new
         current candle to appear.
-        '''
+        """
         if self.mode == 'backtest':
             if (tick - self.last_tick) >= self.timeframe_delta[self.tick_tf]:
                 self.last_tick = tick
@@ -70,26 +68,26 @@ order in length.")
         elif self.mode == 'live':
             return True
         else:
-            raise ValueError("mode must be set to 'backtest' or 'live'")
+            raise ValueError("Strategy mode must be set to 'backtest' or "
+                             "'live'")
 
     def _tf_time_check(self, tick, tf):
-        '''
-        Check if enough ticks have passed for new tf candle or new current
+        """Check if enough ticks have passed for new tf candle or new current
         candle to appear.
-        '''
+        """
         if self.mode == 'backtest':
-            if settings.GET_INCOMPLETE_CANDLES or \
-            (tick - self.last_ticks[tf]) >= self.timeframe_delta[tf]:
+            if settings.GET_INCOMPLETE_CANDLES or (
+                        tick - self.last_ticks[tf]) >= \
+                    self.timeframe_delta[tf]:
                 self.last_ticks[tf] = tick
                 return True
         else:
-            self.tick_tf_time_check(tick, mode=mode)
+            self._tick_tf_time_check(tick)
 
     def _compare_dates(self, df, tf):
-        '''
-        Check if new candles came in, otherwise there is a lapse in
+        """Check if new candles came in, otherwise there is a lapse in
         trading data and no new annotations should be done.
-        '''
+        """
         has_changes = False
         if df.empty:
             return has_changes
@@ -101,21 +99,21 @@ order in length.")
 
         return has_changes
 
-    def _has_changes(self, df, tf, include_current=settings.GET_INCOMPLETE_CANDLES):
-        '''
-        Check if new candles came in, otherwise there is a lapse in
+    def _has_changes(self, df, tf,
+                     include_current=settings.GET_INCOMPLETE_CANDLES):
+        """Check if new candles came in, otherwise there is a lapse in
         trading data and no new annotations should be done.
 
         In backtesting, changes for H1 and H2 timeframes are based on changes
         on M5. This function helps gain minor time benefits from this fact.
-        '''
+        """
         if include_current and self.mode == 'backtest':
             if tf == self.tick_tf:
                 self.tick_tf_changes = self._compare_dates(df, tf)
                 has_changes = self.tick_tf_changes
             elif tf in {'H1', 'H2'}:
                 has_changes = self.tick_tf_changes
-            else: ## when tf is 'M15' for example
+            else:  # When tf is 'M15' for example
                 has_changes = self._compare_dates(df, tf)
         else:
             has_changes = self._compare_dates(df, tf)
@@ -123,7 +121,7 @@ order in length.")
         return has_changes
 
     def tick(self, tick):
-        '''Process strategy at each tick'''
+        """Process strategy at each tick"""
         position_ops = None
 
         if self._tick_tf_time_check(tick):
@@ -131,10 +129,10 @@ order in length.")
 
             for tf in self.timeframes:
                 if self._tf_time_check(tick, tf):
-                    ## query buffer_size of candles
-                    start = tick - \
-                        ((self.buffer_size + 1) * self.timeframe_delta[tf]) + \
-                        self.time_delta
+                    # Query buffer_size of candles
+                    start = tick - (
+                        (self.buffer_size + 1) * self.timeframe_delta[tf]
+                    ) + self.time_delta
                     df = self.broker.get_history(
                         instrument=self.instrument,
                         granularity=tf,
@@ -144,7 +142,7 @@ order in length.")
                         include_current=settings.GET_INCOMPLETE_CANDLES,
                     )
 
-                    ## check if there is a lapse in trading data
+                    # Check if there is a lapse in trading data
                     if self._has_changes(df, tf):
                         has_changes = True
                         self.feeds[tf] = self.annotate_data(df, tf)
@@ -183,9 +181,7 @@ order in length.")
         return feed
 
     def find_open_signal(self, feeds, tick):
-        '''
-        Open a position at time tick.
-        '''
+        """Open a position at time tick."""
         if not tick:
             raise ValueError('Tick value is required for this strategy.')
 
@@ -195,9 +191,7 @@ order in length.")
             return [Open(self, side='buy', price=price)]
 
     def find_close_signal(self, feeds, tick):
-        '''
-        Close a position at time tick, if a positon exists.
-        '''
+        """Close a position at time tick, if a positon exists."""
         for key in feeds:
             price = feeds[key].tail(1)
 
